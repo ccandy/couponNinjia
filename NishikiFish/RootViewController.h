3b0cc45859b17519d4dd5ce9f47239d0daec9310
hua dong <huadong@hua-dongmatoMacBook.local>
2012-08-04 15:51:57 +0800

    Initial Commit

---
+++ NishikiFish/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  RootViewController.h
+//  NishikiFish
+//
+//  Created by dong hua on 12-8-4.
+//  Copyright __MyCompanyName__ 2012年. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RootViewController : UIViewController {
+
+}
+
+@end
